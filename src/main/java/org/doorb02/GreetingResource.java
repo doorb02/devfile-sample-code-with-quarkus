6eb76e972fb8ca8ed972f9938427b1df96590d8b
Bert Doornbusch <bert.doornbusch@gmail.com>
2023-01-06 14:05:12 +0100

    REST endpoint stocks not visible in openshift - stocks resource added to GreetingsResource

--- src/main/java/org/doorb02/GreetingResource.java
+++ src/main/java/org/doorb02/GreetingResource.java
@@ -1,16 +1,38 @@
-//package org.doorb02;
-//
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.core.MediaType;
-//
-//@Path("/hello")
-//public class GreetingResource {
-//
-//    @GET
-//    @Produces(MediaType.TEXT_PLAIN)
-//    public String hello() {
-//        return "Hello RESTEasy";
-//    }
-//}
+package org.doorb02;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+
+    @Path("/{symbol}/latestPrice")
+    @GET
+    public Response stock(@PathParam("symbol") String symbol) throws IOException {
+        Stock stock = YahooFinance.get(symbol);
+
+        Response response = null;
+        if (stock == null) {
+            response = Response.status(Response.Status.NOT_FOUND).build();
+        } else {
+            StockLatestPriceResponse stockResponse = new StockLatestPriceResponse(stock.getSymbol(), stock.getQuote().getPrice());
+            response = Response.ok(stockResponse).build();
+        }
+        return response;
+    }
+
+}
