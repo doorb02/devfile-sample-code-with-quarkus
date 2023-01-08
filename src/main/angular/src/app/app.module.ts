0a7ddde9450d69984e38a70e2e07b4f0d54e9405
Bert Doornbusch <bert.doornbusch@gmail.com>
2023-01-08 15:22:25 +0100

    added angular front end - tested with ng serve

---
+++ src/main/angular/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {ButtonModule} from "primeng/button";
+import {CalendarModule} from "primeng/calendar";
+import {ChartModule} from "primeng/chart";
+import {DialogModule} from "primeng/dialog";
+import {DropdownModule} from "primeng/dropdown";
+import {FileUploadModule} from "primeng/fileupload";
+import {InputTextModule} from "primeng/inputtext";
+import {MenuModule} from "primeng/menu";
+import {OverlayPanelModule} from "primeng/overlaypanel";
+import {PanelModule} from "primeng/panel";
+import {TableModule} from "primeng/table";
+import {ToastModule} from "primeng/toast";
+import {MessageService} from "primeng/api";
+import {SidebarModule} from "primeng/sidebar";
+import {SelectButtonModule} from "primeng/selectbutton";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioComponent
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, HttpClientModule,
+    AppRoutingModule,
+
+    // PrimeNG
+    ButtonModule, CalendarModule, ChartModule, DialogModule, DropdownModule, FileUploadModule, InputTextModule,
+    MenuModule, OverlayPanelModule, PanelModule, SelectButtonModule, SidebarModule, TableModule, ToastModule
+
+  ],
+  providers: [
+    // PrimeNg
+    MessageService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/main/angular/src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit } from '@angular/core';
+import {StockService} from "../stock.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import * as moment from "moment";
+
+export class PortfolioRow {
+  rowIndex: number;
+  symbol: string;
+  name: string;
+  amount: number;
+  currency: string;
+  buyDate: Date;
+  buyPrice: number;
+  buyValue: number;
+  latestDate: Date;
+  latestPrice: number;
+  latestValue: number;
+}
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent implements OnInit {
+
+  portfolioRows: PortfolioRow[];
+
+  constructor(private stockService: StockService) { }
+
+  ngOnInit() {
+    let row1 = new PortfolioRow();
+    row1.rowIndex = 0;
+    row1.symbol = "AAPL";
+    row1.amount = 10;
+    row1.currency = "USD";
+    // row1.buyDate = ;
+    row1.buyPrice = 12.34;
+    row1.buyValue = row1.amount * row1.buyPrice;
+    // row1.latestDate = ;
+    // row1.latestPrice = 12.34;
+    // row1.latestValue = row1.amount * row1.buyPrice;
+    let row2 = new PortfolioRow();
+    row2.rowIndex = 0;
+    row2.symbol = "GOOG";
+    row2.amount = 15;
+    row2.currency = "USD";
+    // row1.buyDate = ;
+    row2.buyPrice = 123.45;
+    row2.buyValue = row2.amount * row2.buyPrice;
+    // row1.latestDate = ;
+    // row1.latestPrice = 12.34;
+    // row1.latestValue = row1.amount * row1.buyPrice;
+    this.portfolioRows = [row1, row2];
+  }
+
+  refreshPortfolioPrices(): void {
+    this.retrieveLatestPrice(this.portfolioRows, () => {});
+  }
+
+  /**
+   * Retrieve latest prices for stocks with keys.
+   * @param keys List of stock keys.
+   * @param stockMap Map with stocks, indexed with keys.
+   * @param onCompleted Callback when retrieval is complete.
+   */
+  private retrieveLatestPrice(rows: PortfolioRow[], onCompleted: () => void) {
+    if (rows.length > 0) {
+      let row: PortfolioRow = rows[0];
+      this.stockService.getStockLatestPrice(row.symbol).subscribe(response => {
+          let date = moment().startOf("day").toDate()
+          row.latestDate = date;
+          row.latestPrice = response.latestPrice;
+          row.latestValue = row.amount * row.latestPrice;
+          this.retrieveLatestPrice(rows.slice(1), onCompleted);
+        },
+        (error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            this.retrieveLatestPrice(rows.slice(1), onCompleted);
+          } else {
+            onCompleted();
+          }
+        },
+        () => {}
+      );
+    } else {
+      onCompleted();
+    }
+  }
+}
